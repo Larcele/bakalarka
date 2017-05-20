4fcc79fd70dc92e68ac29d30d184b51e23386093
Larcele <janka.harvanova@gmail.com>
2017-05-21 00:53:57 +0200

    ThreadHelper extracted to separate class file, HPA partial code separated. Todo: cluster connections for HPA*

---
+++ Bak/Bak/MainWindow_HPA.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bak
+{
+    partial class MainWindow : Form
+    {
+        private void StartHPAstarSearch(object sender, DoWorkEventArgs e)
+        {
+            //do partial a* searches in clusters and visualise them
+
+            //concat it all to pathfindingSolution
+
+            StartAstarSearch(null, null);
+
+        }
+
+        private void BuildHPAClusters()
+        {
+            HierarchicalGraph.Clear();
+
+            AbstractionLayer layer = new AbstractionLayer(0, new List<Cluster>());
+
+            //we make 10 x 10 clusters , from the pper left to the lower right.
+            if (gMap.Width >= 20 && gMap.Height >= 20)
+            {
+                int i = 0;
+                int j = 0;
+                for (j = 0; j <= gMap.Height / HPACsize; ++j)
+                {
+                    for (i = 0; i < gMap.Width; i += HPACsize)
+                    {
+                        CreateHPACluster(layer, i, j);
+                    }
+                }
+            }
+            else //otherwise, we divide the map to 4 clusters by the size of width / 2 x height / 2
+            {
+                int i = 0;
+                int j = 0;
+                int tmpClusterW = gMap.Width / 2;
+                int tmpClusterH = gMap.Height / 2;
+
+                for (j = 0; j <= gMap.Height / tmpClusterH; ++j)
+                {
+                    for (i = 0; i < gMap.Width; i += tmpClusterW)
+                    {
+                        CreateHPACluster(layer, i, j);
+                    }
+                }
+            }
+
+            HierarchicalGraph.Add(layer.ID, layer);
+
+            #region shite
+            /*int xDiv = gMap.Width / 2;
+            int yDiv = gMap.Height / 2;
+
+            int i;
+            int j;
+
+            Cluster c1 = new Cluster(0);
+            Cluster c2 = new Cluster(1);
+            Cluster c3 = new Cluster(2);
+            Cluster c4 = new Cluster(3);
+
+            List<int> inNodes = new List<int>();
+
+            //upper left cluser
+            for (i = 0; i < yDiv * gMap.Width; i += gMap.Width)
+            {
+                for (j = 0; j < xDiv; ++j)
+                {
+                    inNodes.Add(j + i);
+                    if (firstRow(i, 0) ||
+                        lastRow(i, yDiv * gMap.Width) ||
+                        firstCol(j, 0) ||
+                        lastCol(j, xDiv - 1)) { //this node could be one of the outer nodes later
+                        if (gMap.Nodes[i + j].IsTraversable())
+                        {
+                            c1.OuterNodes.Add(i + j);
+                        }
+                    }
+                }
+            }
+            c1.SetInnerNodes(inNodes);
+            inNodes.Clear();
+            //lower left cluster
+            for (i = yDiv * gMap.Width; i < gMap.Height * gMap.Width; i += gMap.Width)
+            {
+                for (j = 0; j < xDiv; ++j)
+                {
+                    inNodes.Add(j + i);
+                    if (firstRow(i, yDiv * gMap.Width) ||
+                        lastRow(i, gMap.Height * gMap.Width) ||
+                        firstCol(j, 0) ||
+                        lastCol(j, xDiv - 1)) { //this node could be one of hte outer nodes later
+                        if (gMap.Nodes[i + j].IsTraversable())
+                        {
+                            c2.OuterNodes.Add(i + j);
+                        }
+                    }
+                }
+            }
+            c2.SetInnerNodes(inNodes);
+            inNodes.Clear();
+            //upper right cluster
+            for (i = 0; i < yDiv * gMap.Width; i += gMap.Width)
+            {
+                for (j = xDiv; j < gMap.Width; ++j)
+                {
+                    inNodes.Add(j + i);
+                    if (firstRow(i, 0) ||
+                        lastRow(i, yDiv * gMap.Width) ||
+                        firstCol(j, xDiv) ||
+                        lastCol(j, gMap.Width - 1)) { //this node could be one of hte outer nodes later
+                        
+                        if (gMap.Nodes[i + j].IsTraversable())
+                        {
+                            c3.OuterNodes.Add(i + j);
+                        }
+                    }
+                }
+            }
+            c3.SetInnerNodes(inNodes);
+            inNodes.Clear();
+            //lower right cluster
+            for (i = yDiv * gMap.Width; i < gMap.Height * gMap.Width; i += gMap.Width)
+            {
+                for (j = xDiv; j < gMap.Width; ++j)
+                {
+                    inNodes.Add(j + i);
+                    if (firstRow(i, yDiv * gMap.Width) ||
+                        lastRow(i, gMap.Height * gMap.Width) ||
+                        firstCol(j, xDiv) ||
+                        lastCol(j, gMap.Width - 1) ) { //this node could be one of hte outer nodes later
+                    
+                        if (gMap.Nodes[i + j].IsTraversable())
+                        {
+                            c4.OuterNodes.Add(i + j);
+                        }
+                    }
+                }
+            }
+            c4.SetInnerNodes(inNodes);
+            inNodes.Clear();
+
+            List<Cluster> clusters = new List<Cluster> { c1, c2, c3, c4 };
+            AbstractionLayer absl = new AbstractionLayer(0, clusters);
+
+            HierarchicalGraph.Add(0, absl);
+
+            BuildClusterConnections(absl);*/
+            #endregion
+        }
+
+        public void CreateHPACluster(AbstractionLayer layer, int columnPos, int rowPos)
+        {
+            Cluster c = new Cluster(layer.LastAssignedClusterID, new List<int>(), new List<int>());
+            layer.LastAssignedClusterID++;
+
+            List<int> innerNodes = new List<int>();
+            List<int> outerNodes = new List<int>();
+
+            int startY = rowPos * HPACsize * gMap.Width;
+            int endY = startY == 0 ? HPACsize * gMap.Width : 2 * startY;
+
+            int startX = columnPos;
+            int endX = columnPos + HPACsize; //non-inclusive
+
+            for (int j = startY; j < endY; j += gMap.Width)
+            {
+                for (int i = startX; i < endX; ++i)
+                {
+                    if (gMap.Nodes.ContainsKey(i + j))
+                    {
+                        innerNodes.Add(i + j);
+                        if (j == startY || i == startX || i == endX - 1 || j + gMap.Width > endY)
+                        {
+                            outerNodes.Add(i + j);
+                        }
+                    }
+
+                }
+            }
+            c.SetInnerNodes(innerNodes);
+            c.SetOuterNodes(outerNodes);
+            layer.Clusters.Add(c.ID, c);
+        }
+
+        private void BuildClusterConnections(AbstractionLayer absl)
+        {
+            foreach (var cnode in absl.Clusters)
+            {
+
+            }
+        }
+
+        #region probably deprecated soon
+
+
+        private bool firstCol(int col, int first)
+        {
+            return col == first;
+        }
+
+        private bool lastCol(int col, int last)
+        {
+            return col == last;
+        }
+
+        private bool lastRow(int row, int last)
+        {
+            return row <= last && row >= last - gMap.Width;
+        }
+
+        private bool firstRow(int row, int first)
+        {
+            return row == first;
+        }
+
+
+        #endregion
+
+    }
+}
